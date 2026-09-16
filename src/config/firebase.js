045a8e89d9947ca2495914077889d90158fd1800
sanguchito <2017razor2017@gmail.com>
2026-09-16 09:06:20 -0600

    feat: 50% completado

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,34 +2,31 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, test} from '@env';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID    
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
 };
 
-console.log("Valor de test: ", test);
 console.log("Valor de configuracion", firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 if (app) {
-  console.log('Firebase initialized successfully');
+  console.log("Firebase initialized successfully");
 } else {
-  console.log('Firebase initialization failed');
+  console.log("Firebase initialization failed");
 }
 
 const database = getFirestore(app);
 if (database) {
-  console.log('Firestore initialized correctly');
+  console.log("Firestore initialized correctly");
 } else {
-  console.log('Firestore initialization failed');
+  console.log("Firestore initialization failed");
 }
 
 // Auth con persistencia en AsyncStorage (mantiene la sesión al cerrar la app)
